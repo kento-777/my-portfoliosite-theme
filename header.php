a4856e766a2db7e735910f9a1bbe07e635173e9a
土田健斗 <kentuuuwork@gmail.com>
2022-01-08 15:04:33 +0900

    add sass file

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
       href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
       rel="stylesheet"
     />
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/styles/css/style.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/index.js"></script>
     <title>Kento Tsuchida</title>
